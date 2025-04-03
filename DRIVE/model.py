e5693b88b0f875c8a3be9f0d444c166f275a9c5c
PonChick3n <aponchick3n420@mail.ru>
2025-04-03 20:37:22 +0300

    showcase results

--- DRIVE/model.py
+++ DRIVE/model.py
@@ -47,26 +47,25 @@ class Unet(nn.Module):
         self.sigmoid = nn.Sigmoid()
         
         # get some layer from resnet to make skip connection
-        self.layer1 = self.resnet.layer1
-        self.layer2 = self.resnet.layer2
-        self.layer3 = self.resnet.layer3
-        self.layer4 = self.resnet.layer4
+        self.layer1 = self.resnet.layer1 # 64
+        self.layer2 = self.resnet.layer2 # 128
+        self.layer3 = self.resnet.layer3 # 256
+        self.layer4 = self.resnet.layer4 # 512
         
         # convolution layer, use to reduce the number of channel => reduce weight number
-        self.conv_5 = Conv(2048, 512, 1, 1, 0)
-        self.conv_4 = Conv(1536, 512, 1, 1, 0)
-        self.conv_3 = Conv(768, 256, 1, 1, 0)
-        self.conv_2 = Conv(384, 128, 1, 1, 0)
-        self.conv_1 = Conv(128, 64, 1, 1, 0)
+        self.conv_4 = Conv(512, 512, 1, 1, 0)  
+        self.conv_3 = Conv(512, 256, 1, 1, 0)   
+        self.conv_2 = Conv(256, 128, 1, 1, 0)   
+        self.conv_1 = Conv(128, 64, 1, 1, 0)    
         # self.conv_0 = Conv(32, 1, 3, 1, 1)
         
         self.out = nn.Conv2d(32, n_classes, 1)
         
         # deconvolution layer
-        self.deconv4 = Deconv(512, 512, 4, 2, 1)
-        self.deconv3 = Deconv(512, 256, 4, 2, 1)
-        self.deconv2 = Deconv(256, 128, 4, 2, 1)
-        self.deconv1 = Deconv(128, 64, 4, 2, 1)
+        self.deconv4 = Deconv(512, 256, 4, 2, 1)
+        self.deconv3 = Deconv(256, 128, 4, 2, 1)
+        self.deconv2 = Deconv(128, 64, 4, 2, 1)
+        self.deconv1 = Deconv(64, 64, 4, 2, 1)
         self.deconv0 = Deconv(64, 32, 4, 2, 1)
 
         
@@ -74,38 +73,37 @@ class Unet(nn.Module):
         x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        skip_1 = x
+        skip_1 = x # 64
         
         x = self.maxpool(x)
         x = self.layer1(x)
-        skip_2 = x
+        skip_2 = x # 64
 
         x = self.layer2(x)
-        skip_3 = x
+        skip_3 = x # 128
         x = self.layer3(x)
-        skip_4 = x
+        skip_4 = x # 256
         
-        x5 = self.layer4(x)
-        x5 = self.conv_5(x5)
+        x5 = self.layer4(x) # 512
+        x5 = self.conv_4(x5) # 512
         
-        x4 = self.deconv4(x5)
+        x4 = self.deconv4(x5) # 256
         x4 = torch.cat([x4, skip_4], dim=1)
-        x4 = self.conv_4(x4)
+        x4 = self.conv_3(x4) # 256
         
-        x3 = self.deconv3(x4)
+        x3 = self.deconv3(x4) # 128
         x3 = torch.cat([x3, skip_3], dim=1)
-        x3 = self.conv_3(x3)
+        x3 = self.conv_2(x3) # 128
         
-        x2 = self.deconv2(x3)
+        x2 = self.deconv2(x3) # 64
         x2 = torch.cat([x2, skip_2], dim=1)
-        x2 = self.conv_2(x2)
+        x2 = self.conv_1(x2) # 64
         
-        x1 = self.deconv1(x2)
+        x1 = self.deconv1(x2) # 64
         x1 = torch.cat([x1, skip_1], dim=1)
-        x1 = self.conv_1(x1)
+        x1 = self.conv_1(x1) # 64
         
-        x0 = self.deconv0(x1)
+        x0 = self.deconv0(x1) # 32
         x0 = self.out(x0)
         
-        x0 = self.sigmoid(x0)
         return x0
--- DRIVE/train.py
+++ DRIVE/train.py
@@ -1,29 +1,11 @@
 import torch
-import torchvision.transforms.v2 as tfs
 import torch.utils.data as data
 import torch.optim as optim
 import torch.nn as nn
 from model import Unet
+from dataprep import DriveDataset
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-class DriveDataset(data.Dataset):
-
-    def __init__(self, images, masks):
-        self.images = images
-        self.masks = masks
-        self.length = len(images)
-        self.transform = tfs.ToDtype(torch.float32, scale=True)
-    
-    def __getitem__(self, item):
-        image = self.transform(self.images[item]).to(device)
-        mask = self.transform(self.masks[item]).to(device)
-        return (image, mask)
-    
-    def __len__(self):
-        return self.length
-
 
 train_images = torch.load('datasets/train_images.pt', weights_only=False)
 train_masks = torch.load('datasets/train_masks.pt', weights_only=False)
@@ -33,31 +15,36 @@ test_masks = torch.load('datasets/test_masks.pt', weights_only=False)
 d_train = DriveDataset(train_images, train_masks)
 d_test = DriveDataset(test_images, test_masks)
 
-train_data = data.DataLoader(d_train, batch_size=4, shuffle=True)
-test_data = data.DataLoader(d_test, batch_size=len(d_test), shuffle=False)
+if __name__ == '__main__':
 
-model = Unet().to(device)
-model.train()
+    train_data = data.DataLoader(d_train, batch_size=4, shuffle=True)
+    test_data = data.DataLoader(d_test, batch_size=len(d_test), shuffle=False)
 
-optimizer = optim.Adam(params=model.parameters(), lr=0.01, weight_decay=0.001)
-loss_func = nn.BCELoss()
-n_epochs = 5
+    model = Unet().to(device)
+    model.train()
 
-for _ in range(n_epochs):
+    optimizer = optim.Adam(params=model.parameters(), lr=0.01, weight_decay=0.001)
+    loss_func = nn.BCEWithLogitsLoss()
+    n_epochs = 5
 
-    for x_train, y_train in train_data:
-        predict = model(x_train).squeeze()
-        loss = loss_func(predict, y_train)
+    for _ in range(n_epochs):
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        for x_train, y_train in train_data:
+            predict = model(x_train).squeeze()
+            loss = loss_func(predict, y_train)
 
-model.eval()
-with torch.no_grad():
-    for x_test, y_test in test_data:
-        predict = model(x_test).squeeze()
-        predict = torch.where(predict >= 0.5, torch.tensor(1.0), torch.tensor(0.0))
-        score = (predict == y_test).float().mean()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+    model.eval()
+    with torch.no_grad():
+        for x_test, y_test in test_data:
+            predict = model(x_test).squeeze()
+            predict = torch.where(predict >= 0.5, torch.tensor(1.0), torch.tensor(0.0))
+            score = (predict == y_test).float().mean()
         
-print(score)
+    print(score.item())
+
+    st = model.state_dict()
+    torch.save(st, 'model_unet_seg.tar')
